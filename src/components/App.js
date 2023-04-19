294addff5e8b5d84dd9672c5e3e76b3bc52060fb
Luna Daniels <aleldaproductions@gmail.com>
2023-04-19 15:13:43 -0600

    mostly finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,41 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  useEffect(()=>{
+    fetch('http://localhost:3001/toys')
+      .then((res)=>res.json())
+      .then((data)=> setToys(data))
+  }, [])
+
+  function handleToyAddition(toy){
+    setToys([...toys, toy])
+  }
+
+  function handleDelete(id){
+    setToys(toys.filter((toy)=>toy.id !== id))
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: 'DELETE'
+    })
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm handleToyAddition={handleToyAddition}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} handleDelete={handleDelete} />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, handleDelete }) {
+
+
+
+
+
+  const showToys = toys.map((toy)=>{
+    return <ToyCard 
+      key={toy.id}
+      id={toy.id}
+      name={toy.name}
+      image={toy.image}
+      likes={toy.likes}
+      onDelete={handleDelete}
+    />
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{showToys}</div>
   );
 }
 
